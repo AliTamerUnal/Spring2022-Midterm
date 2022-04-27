f6bda427fb56b2af72351588a1eeb2fd5e82540c
AliTamerUnal <73219839+AliTamerUnal@users.noreply.github.com>
2022-04-27 09:14:33 +0300

    Update ie201 - Midterm - Q2.py

--- ie201 - Midterm - Q2.py
+++ ie201 - Midterm - Q2.py
@@ -1,8 +1,8 @@
 ﻿"""
 	(25 points)
 
-	Write a recursive function such that, given a list of intergers (say v) and an integer value (say x), the function returns true if
-	there is a subset of intergers in v that sum up to x. Otherwise the function returns false;
+	Write a recursive function such that, given a list of intergers (say v) and an integer value (say x), the function returns resulting subset if
+	there is a subset of intergers in v that sum up to x. Otherwise the function returns an empty list;
 
 	Also write a sample code that runs this function using an example list and value x, and state the expected result.
 """
